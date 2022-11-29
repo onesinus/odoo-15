9393fc7681cea45dd554cabc90ff98576d180a1e
root <root@ones-devbox.asia-southeast2-a.c.delman-internal.internal>
2022-11-29 08:34:48 +0000

    Pass item _status field from sales order to invoice line

--- addons/_delman_sales_order/models/account_move.py
+++ addons/_delman_sales_order/models/account_move.py
@@ -1,7 +1,27 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class DelmanAccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    jagung = fields.Text()
+    item_status = fields.Selection(
+        [('commercial', 'Commercial'), ('warranty', 'Warranty')], 
+        'Item Status', 
+        default='commercial',
+        required=True
+    )
+    
+    @api.onchange('item_status')
+    def onchange_item_status(self):
+        if self.item_status == "warranty":
+            self.price_unit = 0
+        else:
+            self.price_unit = self.product_id.list_price
 
+    # @api.model
+    # def create(self, values):
+    #     """Override default Odoo create function and extend."""
+    #     # Do your custom logic here
+    #     self = self.with_context(check_move_validity=False)
+
+    #     # values["item_status"] = "warranty"
+    #     return super(DelmanAccountMoveLine, self).create(values)
--- addons/_delman_sales_order/models/sale_order.py
+++ addons/_delman_sales_order/models/sale_order.py
@@ -1,7 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class DelmanSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    jagung = fields.Text()
+    item_status = fields.Selection(
+        [('commercial', 'Commercial'), ('warranty', 'Warranty')], 
+        'Item Status', 
+        default='commercial',
+        required=True
+    )
 
+    def _set_price_unit(self, obj):
+        return 0 if obj.item_status == "warranty" else obj.product_id.list_price
+
+    @api.onchange('item_status')
+    def onchange_item_status(self):
+        self.price_unit = self._set_price_unit(self)
+
+    def _prepare_invoice_line(self, sequence):
+        res = super(DelmanSaleOrderLine, self)._prepare_invoice_line()
+        res.update({ 'item_status': self.item_status })
+        return res
